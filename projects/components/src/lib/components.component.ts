23b28b21b35a8c0bf0028105a0e9c171f26ea7eb
jason <jason@zgwit.com>
2023-06-18 08:54:15 +0800

    提交工程

---
+++ projects/components/src/lib/components.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mu-components',
+  template: `
+    <p>
+      components works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ComponentsComponent {
+
+}
